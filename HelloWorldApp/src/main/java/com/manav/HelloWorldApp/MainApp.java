41216beed8c097f3b91b183b41070029c7de37b2
Manav Sharma <mshar246@my.centennialcollege.ca>
2025-02-21 23:38:19 -0500

    good morning and afternoon function is added

--- HelloWorldApp/src/main/java/com/manav/HelloWorldApp/MainApp.java
+++ HelloWorldApp/src/main/java/com/manav/HelloWorldApp/MainApp.java
@@ -1,7 +1,11 @@
 package com.manav.HelloWorldApp;
 
 public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Com367");
-    }
+	public static void main(String[] args) {
+		if (System.currentTimeMillis() >= 0) {
+			System.out.println("Good morning! Welcome to Com367");
+		} else {
+			System.out.println("Good afternoon! Welcome to Com367");
+		}
+	}
 }
